f2ef2b229882cc1f26611bddd93d89fa48fc92d9
Ezra Taimuty-Loomis <etaimutyloomis@gmail.com>
2020-12-31 14:41:41 -0500

    Build script changes

---
+++ install_prereqs.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Install prerequisites for building
+sudo apt install -y g++ git build-essential cmake libboost-all-dev libliquid1d libliquid-dev libsoapysdr-dev soapysdr-module-rtlsdr librtlsdr0 libpulse-dev libasound2-dev libjack-dev protobuf-compiler libprotoc-dev libssh-dev
